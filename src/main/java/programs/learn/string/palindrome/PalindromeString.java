e2adccb399d553e40941c3b477875982165b3d91
Sailesh <Sailesh@DESKTOP-N95E8AK>
2019-02-26 15:46:16 +0530

    String palindrome example

---
+++ src/main/java/programs/learn/string/palindrome/PalindromeString.java
@@ -0,0 +1,25 @@
+package programs.learn.string.palindrome;
+
+import java.util.Scanner;
+
+public class PalindromeString {
+
+	public static void main(String[] args) {
+		String reverse="";
+		Scanner sc=new Scanner(System.in);
+		System.out.println("enter the string");
+		String str=sc.nextLine();
+		for(int i=str.length()-1;i>=0;i--) {
+			reverse=reverse+str.charAt(i);
+		}
+		if(reverse.equals(str)) {
+			System.out.println("palindrome");
+		}
+		else {
+			System.out.println("not palindrome");
+		}
+		sc.close();
+
+	}
+
+}
